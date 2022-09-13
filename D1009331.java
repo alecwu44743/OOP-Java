1d2c58930df2b4481c2db729b7f2d30c7797571c
alecwu44743 <alec.wu44743@gmail.com>
2022-09-13 22:37:53 +0800

    D1009331

---
+++ D1009331.java
@@ -0,0 +1,38 @@
+public class D1009331{
+    public static void main(String[] args){
+        // FCUIECS
+        System.out.println("FFFFFFFF CCCCCCCC UU  UU II EEEEEEEE CCCCCCCC SSSSSSSS");
+        System.out.println("FF       CC       UU  UU II EE       CC       SS    SS");
+        System.out.println("FF       CC       UU  UU II EE       CC       SS");
+        System.out.println("FFFFFF   CC       UU  UU II EEEEEEE  CC       SSSSSSSS");
+        System.out.println("FF       CC       UU  UU II EE       CC             SS");
+        System.out.println("FF       CC       UU  UU II EE       CC             SS");
+        System.out.println("FF       CCCCCCCC  UUUU  II EEEEEEEE CCCCCCCC SSSSSSSS");
+        System.out.println();
+
+        // ALEC
+        System.out.println("AAAAAAAA  LL        EEEEEEEE  CCCCCCCC");
+        System.out.println("AA    AA  LL        EE        CC");
+        System.out.println("AA    AA  LL        EE        CC");
+        System.out.println("AAAAAAAA  LL        EEEEEEE   CC");
+        System.out.println("AA    AA  LL        EE        CC");
+        System.out.println("AA    AA  LL        EE        CC");
+        System.out.println("AA    AA  LLLLLLLL  EEEEEEEE  CCCCCCCC");
+        System.out.println();
+
+        // D1009331
+        System.out.println("DDDDDD   11 00000000 00000000 99999999 33333333 33333333 11");
+        System.out.println("DD    DD 11 00    00 00    00 99    99       33       33 11");
+        System.out.println("DD    DD 11 00    00 00    00 99    99       33       33 11");
+        System.out.println("DD    DD 11 00    00 00    00 99999999 33333333 33333333 11");
+        System.out.println("DD    DD 11 00    00 00    00       99       33       33 11");
+        System.out.println("DD    DD 11 00    00 00    00       99       33       33 11");
+        System.out.println("DDDDDD   11 00000000 00000000       99 33333333 33333333 11");
+        System.out.println();
+        
+
+        // System.out.println("D1009331");
+        // System.out.println("吳克廷");
+    }
+
+}
